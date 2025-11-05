19f26272109f87aa0e2e4cbe8c05d6bc5b50e2a8
Khushi Roy <roykhushi432@gmail.com>
2025-11-05 16:04:31 +0530

    variables and constants

--- learnings/1/main.go
+++ learnings/1/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import "fmt" //pkg imported from the std lib of go
 
+var name = "Khushi";
+const PI = 3.14;
+//constants grouping for multiple configs 
+const (
+	PORT = 8080;
+	host = "localhost"
+)
 func main() {
+	//shorthand syntax => should always be written in the scope of a function
+	age := 22;
 	fmt.Println("Hello world");
+	fmt.Println(name);
+	fmt.Println(age);
+	fmt.Print(PORT,host);
 }
